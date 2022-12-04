6bc6f08e192ec3c54aa0392e3a6d11c628183673
NataliiaPasaman <natapasaman@gmail.com>
2022-12-05 00:29:22 +0200

    fixed styles Contacts

--- src/components/ItemContactList/ItemContactList.jsx
+++ src/components/ItemContactList/ItemContactList.jsx
@@ -15,13 +15,16 @@ export const ItemContactList = () => {
   const addContactItem = filteredContacts.map(({ id, name, number }) => {
     return (
       <li className={css.contacts__item} key={id}>
-        {name}:
-        <span className={css.contacts__number}> {number}</span>
+        <div className={css.contacts__box}>
+          <h3 className={css.contacts__title}>{name}: </h3>
+          <span className={css.contacts__number}> {number}</span>
+        </div>
+
         <button
           type="button"
           className={css.btnDelete}
           onClick={() => dispatch(deleteContact(id))}
-          >
+        >
           Delete
         </button>
       </li>
